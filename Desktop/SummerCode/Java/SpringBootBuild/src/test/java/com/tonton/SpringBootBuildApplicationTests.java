13b5679924a1d736147463ba57bcee3a998b2908
quynhpn2610 <phuongquynhnguyen.2610@gmail.com>
2021-07-20 15:24:27 +0700

    updated

---
+++ Desktop/SummerCode/Java/SpringBootBuild/src/test/java/com/tonton/SpringBootBuildApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tonton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBuildApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
